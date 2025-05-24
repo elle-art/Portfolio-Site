b4499db44a46799ece4bbd684750eed8dcfcd38c
Elle <d.nicole.stewart@gmail.com>
2025-05-23 21:07:17 -0500

    added sit_work.html

--- gallerypage_functions.js
+++ gallerypage_functions.js
@@ -351,8 +351,8 @@ function setParagraphs(btn) {
     document.getElementById("design_h3").innerText = "Welcome Week 2023";
     document.getElementById("design_h4").innerText =
       "Facebook Banner - Social Media";
-    document.getElementById("designP").innerText =
-      "This Facebook banner was designed for ISU Welcome Week 2023. Designed to capture the essence of campus life, it serves as a visual gateway to a week filled with excitement, collaboration, and the spirit of ISU, setting the tone for a great start to a new school year!";
+    document.getElementById("designP").innerHTML =
+      'This Facebook banner was designed for ISU Welcome Week 2023. Designed to capture the essence of campus life, it serves as a visual gateway to a week filled with excitement, collaboration, and the spirit of ISU, setting the tone for a great start to a new school year! <ul class="page_link"><li id="design_link"><a href="sit_work.html" target="_blank">See more work for the School of IT</a></li></ul>';
   }
   if (btn == "dot4") {
     document.getElementById("design_h3").innerText = "Recruitment Postcards";
@@ -371,7 +371,8 @@ function setImages(btn) {
   }
   if (btn == "dot1") {
     image.setAttribute("src", "gallery_imgs/design/bonfire_frame.png");
-    link.innerHTML = "Open in browser";
+    link.innerHTML =
+      '<a id="bonfire_link" href="https://xd.adobe.com/view/cbbada60-2f54-4054-ab31-a2f2438b3603-9ee9/" target="_blank">Open in browser</a>';
     document.getElementById("bonfire_video").style.opacity = "1";
     document.getElementById("bonfire_video").style.zIndex = "1";
   }
@@ -381,7 +382,8 @@ function setImages(btn) {
   }
   if (btn == "dot3") {
     image.setAttribute("src", "gallery_imgs/design/banner_frame.png");
-    link.innerHTML = " ";
+    link.innerHTML =
+      '<a href="sit_work.html" target="_blank">See more work for the School of IT</a>';
   }
   if (btn == "dot4") {
     image.setAttribute("src", "gallery_imgs/design/postcard_frame.png");
@@ -735,116 +737,6 @@ function setCodeFrame(frame) {
     document.getElementById("wrapBloom").style.opacity = "1";
     document.getElementById("bloom").style.opacity = "1";
     document.getElementById("wrapBloom").style.zIndex = "1";
-  }
-}
-
-function setParagraphs(btn) {
-  let link = document.querySelector("#code_link a");
-  // Remove any existing button container
-  const existingBtnContainer = document.getElementById("cas-collab-btns");
-  if (existingBtnContainer) {
-    existingBtnContainer.remove();
-  }
-
-  if (btn == "btnA") {
-    document.getElementById("code_h3").innerText = "Greetings 101";
-    document.getElementById("code_h5").innerText = "(Play the demo video)";
-    document.getElementById("code_p").innerText =
-      "Greetings 101 is a web-based application designed to help users learn and practice greetings in various languages. Click the link below to explore this project in more detail.";
-    link.href = "about.html#current";
-  }
-
-  if (btn == "btnB") {
-    document.getElementById("code_h3").innerText = "CAS Collaboration System";
-    document.getElementById("code_h5").innerText = "(View page in browser)";
-    document.getElementById("code_p").innerText =
-      "I had the privilege of redesigning the College of Arts and Sciences Collaboration System at ISU. This web-based application is facilitates collaboration among students, faculty, and staff, allowing users to search for professors and explore their research interests. I also designed the Syllabi Database for CAS, which can be viewed through the button below. ";
-
-    link.href = "https://casit.illinoisstate.edu/collaboration-dev";
-
-    // Add buttons for viewing an image and a web page
-    const btnContainer = document.createElement("div");
-    btnContainer.id = "cas-collab-btns";
-    btnContainer.style.marginTop = "10px";
-
-    // Image button
-    const imgBtn = document.createElement("button");
-    imgBtn.innerText = "View Previous Design";
-    imgBtn.onclick = function () {
-      window.open("gallery_imgs/code/Previous_Col_Sys.png", "_blank");
-    };
-    btnContainer.appendChild(imgBtn);
-
-    // Web page button
-    const syllabiBtn = document.createElement("button");
-    syllabiBtn.innerText = "Open CAS Syllabi Database Page";
-    syllabiBtn.style.marginLeft = "8px";
-    syllabiBtn.onclick = function () {
-      window.open("https://casit.illinoisstate.edu/syllabi", "_blank");
-    };
-    btnContainer.appendChild(syllabiBtn);
-
-    // Insert the buttons after the paragraph
-    const codeP = document.getElementById("code_p");
-    codeP.parentNode.insertBefore(btnContainer, codeP.nextSibling);
-  }
-  if (btn == "btn1") {
-    document.getElementById("code_h3").innerText = "ClassStar";
-    document.getElementById("code_h5").innerText = "(Interact with the app)";
-    document.getElementById("code_p").innerText =
-      "A friend of mine mentioned that as a peer mentor, they have to keep track of their student's behaviors. They asked me if that was something I could code. This code allows a user to create a roster of students and a list of behaviors. There's a discrete button mode for moving around the classroom and importing/exporting of excel files to keep track of data over time";
-    link.href = "https://elle-art.github.io/ClassStar/";
-  }
-  if (btn == "btn2") {
-    document.getElementById("code_h3").innerText = "Mp1: Caliente";
-    document.getElementById("code_h5").innerText = "(Click to animate)";
-    document.getElementById("code_p").innerText =
-      "This is Cali...she's a bit of a hot head. No images, just several lines of code create the character, Cali, and the destruction she leaves in her wake. Every detail is meticulously crafted using JavaScript code.";
-    link.href = "https://elle-art.github.io/CTK302/Unit1/mp1/index.html";
-  }
-  if (btn == "btn3") {
-    document.getElementById("code_h3").innerText = "Mp3: Bloom";
-    document.getElementById("code_h5").innerText =
-      "(Use left and right arrow keys to move)";
-    document.getElementById("code_p").innerText =
-      "Bloom into a JavaScript gaming experience! Collect water droplets to grow the flower atop your character's head. Hand-drawn assets, each drawn with Sharpie on notebook paper, add an authentic charm, creating a uniquely personalized aesthetic.";
-    link.href = "https://elle-art.github.io/CTK302/Unit3/mp3/index.html";
-  }
-  if (btn == "btn4") {
-    document.getElementById("code_h3").innerText = "CSS Animation: Trains";
-    document.getElementById("code_h5").innerText = "(Scroll and hover)";
-    document.getElementById("code_p").innerText =
-      "Explore the benefits of public transportation though CSS animations! The GIFs, sourced from the web, influenced the vibrant and futuristic color scheme. Notable features include a hover-triggered animated pie chart that represents key data and a timed animation at the bottom, all to promote public transportation through an aesthetically pleasing and interactive lens.";
-    link.href =
-      "https://elle-art.github.io/CTK303/Assignment2/CSSanimation.html";
-  }
-
-  if (btn == "dot1") {
-    document.getElementById("design_h3").innerText = "Bonfire";
-    document.getElementById("design_h4").innerText =
-      "Interactive App Mockup - Honors Contract";
-    document.getElementById("designP").innerText =
-      "Join the Bonfire! This app unifies communities by highlighting local businesses offering free and low-cost resources. Through Adobe XD, you can interact with this mockup as if the app was downloaded on your phone. Explore it for yourself! ";
-  }
-  if (btn == "dot2") {
-    document.getElementById("design_h3").innerText = "Plantfolio";
-    document.getElementById("design_h4").innerText =
-      "Landing Page - Composition and Design";
-    document.getElementById("designP").innerText =
-      "Learn all about plants with Plantfolio, a plant encyclopedia! This captivating landing page, designed in Adobe InDesign, offers a glimpse into a user-friendly and visually appealing platform for exploring the wondrous world of plants.";
-  }
-  if (btn == "dot3") {
-    document.getElementById("design_h3").innerText = "Welcome Week 2023";
-    document.getElementById("design_h4").innerText =
-      "Facebook Banner - Social Media";
-    document.getElementById("designP").innerText =
-      "This Facebook banner was designed for ISU Welcome Week 2023. Designed to capture the essence of campus life, it serves as a visual gateway to a week filled with excitement, collaboration, and the spirit of ISU, setting the tone for a great start to a new school year!";
-  }
-  if (btn == "dot4") {
-    document.getElementById("design_h3").innerText = "Recruitment Postcards";
-    document.getElementById("design_h4").innerText = "Postcards - Social Media";
-    document.getElementById("designP").innerText =
-      " As part of my job with the School of IT at ISU, I designed mockups for recruitment postcards. These visual representations not only show the proud achievements of the School of IT but also serve as compelling invitations to prospective students.";
   }
 }
 
